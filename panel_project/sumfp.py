8e629f1007169ff1f3f031d8a125e95fa7d75bd3
dazhizhi <chachadeer@gmail.com>
2024-04-25 19:44:53 +0800

    temp fix 2

--- panel_project/sumfp.py
+++ panel_project/sumfp.py
@@ -1,22 +1,30 @@
+# code=UTF-8
 import os
 
-folder_path = 'D:/Mega/fapiao/new'  # Replace with the path to your folder
+folder_path = 'D:/fapiao'  # Replace with the path to your folder
 file_list = os.listdir(folder_path)
 file_list = sorted(file_list)
 
-with open('fapiao.csv','w') as f:
+with open('fapiao.csv', 'w') as f:
+    total = 0
 
-    for filename in file_list:
+    for index, filename in enumerate(file_list):
         name, extension = os.path.splitext(filename)
         split_i = -1
-        for i,letter in enumerate(name):
+        for i, letter in enumerate(name):
             if ord('0') <= ord(letter) <= ord('9'):
                 split_i = i
                 break
 
+        number_str = f"【{index:03d}】"
+        new_filename = number_str + filename
+
         cate = name[:split_i]
-        number = name[split_i:]
+        amount = name[split_i:]
+        total += float(amount)
+        f.write(number_str + ',')
         f.write(cate + ',')
-        f.write(number + '\n')
-
-
+        f.write(amount + '\n')
+        os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
+    total = round(total, 2)
+    f.write(f"[],总计,{total}")
