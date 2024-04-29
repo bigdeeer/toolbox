84bfe2a5eabd2594256812c79f1fe24399435500
Deer <chachadeer@gmail.com>
2024-04-29 17:08:01 +0800

    invoice xml parse 2

--- panel_project/invoice.py
+++ panel_project/invoice.py
@@ -9,11 +9,17 @@ file_list = os.listdir(folder_path)
 xml_set = set()
 pdf_set = set()
 
+
 for file in file_list:
     if ".xml" in file:
         xml_set.add(file)
     elif ".pdf" in file:
         pdf_set.add(file)
+
+pdf_count = 0
+print(len(pdf_set))
+print(len(xml_set))
+new_name_set = set()
 
 for xml_file in xml_set:
     xml_path = os.path.join(folder_path, xml_file)
@@ -22,14 +28,27 @@ for xml_file in xml_set:
 
     total = root.find(".//TotalTax-includedAmount").text
     name = root.find(".//ItemName").text
-    name = name.split("*",1)[1]
-    name = name.rsplit("*",1)[0]
+    name = name.split("*", 1)[1]
+    name = name.split("*", 1)[0]
 
     code_0 = xml_file.rsplit('.xml', 1)[0]
     code = code_0.rsplit('_', 1)[1]
 
+    name_id = name + total
+    if name_id in new_name_set:
+        name += 'B'
+    else:
+        new_name_set.add(name_id)
+
     pdf_source_filename = f"digital_{code}.pdf"
     pdf_source_path = os.path.join(folder_path, pdf_source_filename)
+    if os.path.exists(pdf_source_path):
+        print('existed')
+        pdf_count +=1
+    else:
+        print(f'no pdf found {pdf_source_filename}')
     pdf_dest_filename = name + total + ".pdf"
     pdf_dest_path = os.path.join(dest_path, pdf_dest_filename)
     shutil.copy(pdf_source_path, pdf_dest_path)
+
+print(pdf_count)
