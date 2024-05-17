fcb2ca9071e86ff33298b22814fdf8d17d9b2ac4
dazhizhi <chachadeer@gmail.com>
2024-05-18 00:36:34 +0800

    yd simplify test OK

--- youtube_project/test_parse.py
+++ youtube_project/test_parse.py
@@ -1,34 +1,16 @@
-from pprint import pprint
-import json
-import socket
-
-from urllib import request
-from urllib.request import Request, urlopen
-
 import pytube
+from pprint import pprint
+
+
+
 
 url = 'https://www.youtube.com/watch?v=PLmbeupnGoo'
-
 yt = pytube.YouTube(url, proxies={'https': "socks5://127.0.0.1:4781"})
-streams = yt.fmt_streams
+yt.bypass_age_gate()
+streaming = yt.vid_info['streamingData']
+data1 = streaming['formats']
+data2 = streaming['adaptiveFormats']
+data = data1+data2
+for item in data:
+    pprint(item)
 
-
-
-endpoint = 'https://www.youtube.com/youtubei/v1/player'
-method = 'POST'
-headers = {'Content-Type': 'application/json', 'User-Agent': 'com.google.android.apps.youtube.music/'}
-
-data = {'context': {'client': {'androidSdkVersion': 30, 'clientName': 'ANDROID_MUSIC', 'clientVersion': '5.16.51'}}}
-proxies = {'https': "socks5://127.0.0.1:4781"}
-
-proxy_support = request.ProxyHandler(proxies)
-opener = request.build_opener(proxy_support)
-request.install_opener(opener)
-
-base_headers = {"User-Agent": "Mozilla/5.0", "accept-language": "en-US,en"}
-base_headers.update(headers)
-data = bytes(json.dumps(data), encoding="utf-8")
-request = Request(url, headers=base_headers, method=method, data=data)
-response = urlopen(request, timeout=1000)  # nosec
-
-pprint(response.read())
